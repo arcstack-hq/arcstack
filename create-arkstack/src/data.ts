099ba5ed0dc237fa9b37ca740b8c6712eded6f65
3m1n3nc3 <mygames.ng@gmail.com>
2026-03-10 22:48:47 +0100

    chore: update dependency versions to 0.1.9

--- create-arkstack/src/data.ts
+++ create-arkstack/src/data.ts
@@ -25,12 +25,12 @@ export const depsToRemove = [
 ]
 
 export const depsToAdd = {
-    '@arkstack/console-slim': '^0.1.8',
+    '@arkstack/console-slim': '^0.1.9',
 }
 
 export const depsList = {
-    '@arkstack/common': '^0.1.8',
-    '@arkstack/console': '^0.1.8',
-    '@arkstack/contract': '^0.1.8',
-    '@arkstack/driver-express': '^0.1.8'
+    '@arkstack/common': '^0.1.9',
+    '@arkstack/console': '^0.1.9',
+    '@arkstack/contract': '^0.1.9',
+    '@arkstack/driver-express': '^0.1.9'
 }
