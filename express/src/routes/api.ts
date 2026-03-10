5a095d07ded3bb01f81e705b1cefa2472f473ecc
3m1n3nc3 <mygames.ng@gmail.com>
2026-03-10 17:09:51 +0100

    fix: update /hello route to use res.send for proper response handling

--- express/src/routes/api.ts
+++ express/src/routes/api.ts
@@ -1,8 +1,8 @@
 import { Router } from 'src/core/router'
 import UserController from 'src/app/http/controllers/UserController'
 
-Router.get('/hello', ({ res }) => {
-  res.send('Hello World')
+Router.get('/hello', () => {
+  return ['Hello World']
 })
 
 Router.apiResource('/users', UserController)
