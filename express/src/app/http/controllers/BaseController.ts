0f0aba8a8168698ba7ae2fab3e9999152efbfd67
3m1n3nc3 <mygames.ng@gmail.com>
2026-03-11 18:57:47 +0100

    feat: update validation method to use generic types for improved type safety

--- express/src/app/http/controllers/BaseController.ts
+++ express/src/app/http/controllers/BaseController.ts
@@ -1,10 +1,10 @@
+import { RulesForData, Validator } from 'kanun'
+
 import { Controller } from 'clear-router'
 import { HttpContext } from 'clear-router/types/express'
-import { Validator } from 'kanun'
 
 export class BaseController extends Controller<HttpContext> {
-  // oxlint-disable-next-line typescript/no-explicit-any
-  async validate<X extends Record<string, any>> (rules: X) {
+  async validate<D extends Record<string, any>, R extends RulesForData<D>> (rules: R) {
     return Validator.make(this.body, rules).validate()
   }
 }
