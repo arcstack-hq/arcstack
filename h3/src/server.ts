2f25f1b97c025299799ba5e34dc80298727ed6d7
3m1n3nc3 <mygames.ng@gmail.com>
2026-03-14 22:45:57 +0100

    chore: add APP_PORT to environment files and update server boot configuration

--- h3/src/server.ts
+++ h3/src/server.ts
@@ -1,6 +1,7 @@
 import { app } from './core/bootstrap'
 import { bootWithDetectedPort } from '@arkstack/common'
+import { env } from './core/utils/helpers'
 
 await bootWithDetectedPort(async (port) => {
-  await app.boot(port)
+  await app.boot(parseInt(env('APP_PORT', String(port)), 10))
 })
